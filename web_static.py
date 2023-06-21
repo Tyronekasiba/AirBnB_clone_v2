00e04e0588a4b12724e435d35bfa8b8904a25305
Tyronekasiba <tyronekasiba@gmail.com>
2023-06-21 14:02:53 -0700

    Bug free!

--- web_static.py
+++ web_static.py
@@ -1,25 +1,22 @@
+#!/usr/bin/python3
+# archived files to generates a .tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
-def create_compressed_archive():
-    """Create a compressed archive (.tgz) of the web_static directory."""
-    current_time = datetime.utcnow()
-    archive_filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        current_time.year,
-        current_time.month,
-        current_time.day,
-        current_time.hour,
-        current_time.minute,
-        current_time.second
-    )
-
-    if not os.path.isdir("versions"):
-        if local("mkdir -p versions").failed:
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
             return None
-
-    if local("tar -cvzf {} web_static".format(archive_filename)).failed:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-
-    return archive_filename
+    return file
